7a2e52abb09369d544fa781145b9288a219bb8b1
Bladupsy <gisela.cristaldo.work@gmail.com>
2022-06-28 23:55:48 -0300

    ✨init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Small {
+  category: string,
+  title: string,
+  description: string,
+  price: number,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'challange';
+
+  smallCard: Array<Small> = [{category: 'PERFUME', title: 'Gabrielle Essence Eau De Parfum', description: 'A floral, solar and voluptuous interpretation composed by Oliver Polge, Perfumer-Creator for the House of CHANEL', price: 149.99}]
 }
